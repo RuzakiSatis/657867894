f0e5ae98cb8b1eadded97bd9bef86e18b6b5bdf4
RuzakiSatis <146598189+RuzakiSatis@users.noreply.github.com>
2023-11-26 20:10:58 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Route, Routes  } from "react-router-dom";
+import { Home } from './Home/Home';
+
+
+
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path='/' Component={Home}></Route>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
